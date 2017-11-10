0275b7c41eebf3a735d4f806fcd9dc62bbd6d2ff
melihyayla <melihyayla@gmail.com>
2017-11-11 00:53:32 +0300

    first commit

---
+++ app/src/main/java/com/akillitarifler/yemekhanem/MainActivity.java
@@ -0,0 +1,18 @@
+package com.akillitarifler.yemekhanem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void category(View view){
+        Toast.makeText(this, "DENEME", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
